223c9b2c71e25efc748876f50a6ec1694262e9f0
IDorry <1767771542@qq.com>
2018-11-18 09:28:48 +0800

    小bug处理完成

--- js/isLogin.js
+++ js/isLogin.js
@@ -4,5 +4,12 @@ $.ajax({
     dataType: "json",
     success: function( info ) {
         console.log(info);
+        if( info.success ){
+             // 已登录，继续访问
+            console.log("已登录，继续访问");
+        } 
+        if( info.error == 400 ){
+            location.href = "login.html";
+        }       
     }
 });
